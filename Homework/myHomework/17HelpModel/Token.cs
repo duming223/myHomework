23db5ce0f0b005e762a42b11bdaef2821b11ebf0
gitcatlover <547284251@qq.com>
2019-07-04 20:18:27 +0800

    clear email

---
+++ Homework/myHomework/17HelpModel/Token.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myHomework._17HelpModel
+{
+    [Flags]
+    public enum Token
+    {
+        SuperAdmin = 1,
+        Admin = 2,
+        Blogger = 4,
+        Newbie = 8,
+        Registered = 16
+    }
+}
---
+++ Homework/myHomework/17HelpModel/TokenManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myHomework._17HelpModel
+{
+    public class TokenManager
+    {
+        private IList<string> _tokens;
+        public TokenManager()
+        {
+            _tokens = Get();
+        }
+
+        public static IList<string> Get()
+        {
+            IList<string> enums = new List<string>();
+            foreach (var s in Enum.GetNames(typeof(Token)))
+            {
+                enums.Add(s);
+            }
+            return enums;
+        }
+        public void Add(Token token)
+        {
+            _tokens.Add(token.ToString());
+        }
+        public void Remove(Token token)
+        {
+            _tokens.Remove(token.ToString());
+        }
+        public void Has(Token token)
+        {
+            _tokens.Contains(token.ToString());
+        }
+    }
+}
